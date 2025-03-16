eef01986f79ddf892bb04f6e5a1620b9c146c28e
JesseSBezerra <jessebezerra@hotmail.com.br>
2025-03-16 13:48:57 -0300

    .
    .

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,5 +1,5 @@
 variable "region" { default = "sa-east-1" }
-variable "app_name" { default = "js-ecs-confirmador" }
+variable "app_name" { default = "hub-lancamentos" }
 variable "ecr_image_url" { default = "397685870114.dkr.ecr.sa-east-1.amazonaws.com/hub-lancamentos:latest" }
 variable "container_port" { default = 8080 }
 variable "ecr_repo_name" { default = "hub-lancamentos" }
