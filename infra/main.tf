f6b77046826e3399a4dea65260f27d8368c7da95
JesseSBezerra <jessebezerra@hotmail.com.br>
2025-03-16 19:46:55 -0300

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -79,11 +79,27 @@ resource "aws_iam_role" "ecs_task_execution_role" {
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
     Statement = [{
-      Action    = "sts:AssumeRole",
       Effect    = "Allow",
       Principal = {
         Service = "ecs-tasks.amazonaws.com"
-      }
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "dynamodb_access" {
+  name = "${var.app_name}-dynamodb-access"
+  role = aws_iam_role.ecs_task_execution_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect   = "Allow",
+      Action   = [
+        "dynamodb:*"
+      ],
+      Resource = "*"
     }]
   })
 }
