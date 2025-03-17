630c9fa20a6bfbfec73cc1650be5cb72440d680d
JesseSBezerra <jessebezerra@hotmail.com.br>
2025-03-17 07:38:58 -0300

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -143,6 +143,7 @@ resource "aws_ecs_task_definition" "app_task" {
   cpu                      = "512"
   memory                   = "1024"
   execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  task_role_arn      = aws_iam_role.ecs_task_execution_role.arn
 
   container_definitions = jsonencode([
     {
